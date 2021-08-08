80195d8feaddab11a8b995fe7ce9e2086a6b7321
shes50103 <shes50103@gmail.com>
2021-08-08 20:22:09 +0100

    Basic demo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,13 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    # @posts = Post.all
-    @posts = Post.all.includes(:comments)
+    @posts = Post.all
+    # @posts = Post.all.includes(:comments)
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    go
   end
 
   # GET /posts/new
@@ -66,5 +67,15 @@ class PostsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:body, :name, :status)
+    end
+
+    def go
+      r = 0
+      1000.times do
+        1000.times do
+          r = r + 1
+        end
+      end
+      puts "r: #{r}"
     end
 end
